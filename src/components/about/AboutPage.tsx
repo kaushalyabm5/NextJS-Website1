b01f38f028e1a91bbc3946dde42fbd1a77cce949
Kaushalya <jb740le@gmail.com>
2026-09-21 20:18:42 +0530

    remove unused AboutBrands component

--- src/components/about/AboutPage.tsx
+++ src/components/about/AboutPage.tsx
@@ -3,14 +3,14 @@ import AboutHero from './AboutHero'
 import WhoWeAre from './WhoWeAre'
 import MissionVision from './MissionVision'
 import CoreValues from './CoreValues'
-import AboutBrands from './AboutBands'
+
 
 const AboutPage = () => {
   return (
     <div>
         <AboutHero />
         <WhoWeAre />
-        <AboutBrands />
+    
         {/*<CoreValues /> */}
         {/*<MissionVision /> */}
         
